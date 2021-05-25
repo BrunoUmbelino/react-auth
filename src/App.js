23737f0a90e02b49c99a0ce0b98612ca8948bb0b
Bruno <bruno.umbelino0@gmail.com>
2021-05-25 18:51:59 -0300

    adding react router dom

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import Login from "./pages/Login";
 import Main from "./pages/Main";
+import { Switch, BrowserRouter, Route } from "react-router-dom";
 
 function App() {
   return (
     <>
-      {/* <Login /> */}
-      <Main />
+      <BrowserRouter>
+        <Switch>
+          <Route path="/" exact component={Login} />
+          <Route path="/main" component={Main} />
+        </Switch>
+      </BrowserRouter>
     </>
   );
 }
